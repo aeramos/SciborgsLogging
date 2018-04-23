167543225c84759e49b85aae7964faf97b8cdce4
Alejandro Ramos <alejandro.ramos.work@gmail.com>
2018-04-23 16:34:02 -0400

    Make the logger better

--- src/log/LoggingTest.java
+++ src/log/LoggingTest.java
@@ -1,26 +1,45 @@
 package log;
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.FileHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.logging.XMLFormatter;
+import java.util.logging.*;
+
 public class LoggingTest {
-	final static Logger log = Logger.getGlobal();
-	public static void main(String... args) throws IOException {
-		XMLFormatter formatter = new XMLFormatter();
-		FileHandler handle = new FileHandler("Test.txt");
-		handle.setFormatter(formatter);
-		log.addHandler(handle);
-		log.setLevel(Level.ALL);
-		log.entering(LoggingTest.class.getName(), "testLogging");
-		testLogging();
-		log.exiting(LoggingTest.class.getName(), "testLogging");
-	}
-	public static void testLogging() throws IOException {
-		File f = new File("HELPPPP.txt");
-		if(!f.exists()) {
-			log.log(Level.SEVERE, "FILE={0} does not exist!", new Object[] {"HELPPPP.txt"});
-		}
-	}
+    private final static Logger logger = Logger.getGlobal();
+
+    public static void main(String... args) throws IOException {
+        Handler files = new FileHandler("Test.xml");
+        files.setFormatter(new XMLFormatter());
+
+        Handler console = new ConsoleHandler();
+
+        console.setLevel(Level.ALL);
+        files.setLevel(Level.ALL);
+
+        logger.addHandler(files);
+        logger.addHandler(console);
+        logger.setLevel(Level.ALL);
+        logger.setUseParentHandlers(false);
+
+        logger.entering(LoggingTest.class.getName(), "method");
+        method();
+        logger.exiting(LoggingTest.class.getName(), "method");
+
+        testLogging();
+    }
+
+    public static void testLogging() throws IOException {
+        File f = new File("HELPPPP.txt");
+        if (!f.exists()) {
+            logger.log(Level.SEVERE, "FILE={0} does not exist!", new Object[]{f.getName()});
+            if (f.createNewFile()) {
+                logger.log(Level.CONFIG, "Created new file {0}", new Object[]{f.getName()});
+            }
+        } else {
+            logger.log(Level.FINE, "File exists");
+        }
+    }
+
+    public static void method() {
+        return;
+    }
 }
